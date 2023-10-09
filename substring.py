01929a6634f95e1eb8c4eacd5f2a4b3cf321bda2
Anoosha <116190024+velagana@users.noreply.github.com>
2023-10-09 18:48:45 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,7 @@
+'''Check if String Contains Substring in Python'''
+string = input("enter the string: ")
+sub_string = input("enter the string: ")
+if sub_string in string:
+    print("yes")
+else:
+    print("no")
