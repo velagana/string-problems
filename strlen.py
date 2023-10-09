b4762cc41cad54efd7b902e3f140516e34ad19d1
Anoosha <116190024+velagana@users.noreply.github.com>
2023-10-09 21:33:40 +0530

    Add files via upload

---
+++ strlen.py
@@ -0,0 +1,5 @@
+''' FINDING THE LENGTH OF THE STRING '''
+string = input("Enter the string: ")
+#string = "hellogurupremakosameh jevetham magallatho adadaneke ala pavrusham preminchendhe nenneh kadhantundhe nanneh maha maha sundarangulley pondalene vadne"
+len_string = len(string)
+print(len_string)
