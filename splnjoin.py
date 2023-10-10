14d57be825ecaab1fd180e7aec4b6e9d154a0d3e
Anoosha <116190024+velagana@users.noreply.github.com>
2023-10-10 17:22:59 +0530

    Add files via upload

---
+++ splnjoin.py
@@ -0,0 +1,8 @@
+''' Python program to split and join a string '''
+string1 = input("enter the string: ")
+string_spl = string1.split()
+print(string_spl)
+str_jn = " ".join(string_spl)
+print(str_jn)
+
+
