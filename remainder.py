7e70321ae851927cc50b361c3ef4d73a6ff4b5f6
Anoosha <116190024+velagana@users.noreply.github.com>
2023-11-09 20:23:00 +0530

    Add files via upload

---
+++ remainder.py
@@ -0,0 +1,7 @@
+'''Python Program for Find remainder of array multiplication divided by n'''
+
+arr = [100, 10, 5, 25, 35, 14];lens = len(arr);n = 11
+mul = 1
+for i in range(lens):
+    mul = (mul * (arr[i] % n)) % n
+print(mul % n)
